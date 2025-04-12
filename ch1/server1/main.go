0c6ed21dd43cb3ea0acc8da149ee3250a377b9f2
Daniel Nunes <danielmnunes08@gmail.com>
2025-04-12 09:51:24 -0300

    add server project

---
+++ ch1/server1/main.go
@@ -0,0 +1,17 @@
+// Server1 é um servidor de "eco mínimo"
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	_, _ = fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
